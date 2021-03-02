fbf40f759d12c5d64a47ee493a2d1bf92cdd3d02
mrota89 <marcello.rota.1989@gmail.com>
2021-03-02 15:37:15 +0100

    creata struttura dati nel file database.php e struttura html in index.php

---
+++ database/database.php
@@ -0,0 +1,47 @@
+<?php
+$dischi = [
+  [
+    "copertina" => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+    "titolo" => "New Jersey",
+    "autore" => "Bon Jovi",
+    "anno" => "1988",
+    "genere" => "Rock"
+  ],
+  [
+    "copertina" => "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
+    "titolo" => "Live at Wembley 86",
+    "autore" => "Queen",
+    "anno" => "1992",
+    "genere" => "Pop"
+  ],
+  [
+    "copertina" => "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
+    "titolo" => "Ten's Summoner's Tales",
+    "autore" => "Sting",
+    "anno" => "1993",
+    "genere" => "Pop"
+  ],
+  [
+    "copertina" => "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
+    "titolo" => "Steve Gadd Band",
+    "autore" => "Steve Gadd Band",
+    "anno" => "2018",
+    "genere" => "Jazz"
+  ],
+  [
+    "copertina" => "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
+    "titolo" => "Brave new World",
+    "autore" => "Iron Maiden",
+    "anno" => "2000",
+    "genere" => "Metal"
+  ],
+  [
+    "copertina" => "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
+    "titolo" => "One more car, one more raider",
+    "autore" => "Eric Clapton",
+    "anno" => "2002",
+    "genere" => "Rock"
+  ]
+];
+
+ ?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+  require_once __DIR__ . '/database/database.php';
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+   <head>
+       <meta charset="utf-8">
+       <title>php-ajax-dischi-milestone-1</title>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
+       <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+       <link rel="stylesheet" href="css/app.css">
+   </head>
+   <body>
+     <header>
+       <div class="container">
+         <img src="img/logo.png" alt="logo"/>
+       </div>
+     </header>
+
+     <div class="cds-container container">
+       <?php
+          foreach ($dischi as $disco) {
+        ?>
+       <div class="cd">
+         <img src="<?php echo $disco["copertina"]?>" alt="">
+         <h3><?php echo $disco["titolo"]?></h3>
+         <span class="author"><?php echo $disco["autore"]?></span>
+         <span class="year"><?php echo $disco["anno"]?></span>
+       </div>
+       <?php
+        }
+        ?>
+     </div>
+   </body>
+</html>
